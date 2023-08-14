cae134011e144cf48360512f31d5dc4943cd30bc
Myriam <myriam.mabrouki@etu.sorbonne-universite.fr>
2023-08-14 19:14:48 +0200

    lib and thread

--- scheduler.c
+++ scheduler.c
@@ -1,8 +1,22 @@
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/adpcm_dec/adpcm_dec.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/adpcm_enc/adpcm_enc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+//#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/adpcm_dec/adpcm_dec.h"
+//#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/adpcm_enc/adpcm_enc.h"
+
+#include "/home/myriam/Videos/stage/tacle-bench/bench/librairies/adpcm_dec/adpcm_dec.h"
+#include "/home/myriam/Videos/stage/tacle-bench/bench/librairies/adpcm_enc/adpcm_enc.h"
+#include "/home/myriam/Videos/stage/tacle-bench/bench/librairies/anagram/anagram.h"
 
 int main () {
-    main_adpcm_dec();
-    main_adpcm_enc();
+    
+    pthread_t thA, thB;
+    pthread_create(&thA, NULL, (void *) main_adpcm_dec, NULL);
+    pthread_create(&thB, NULL, (void *) main_anagram, NULL);
+    pthread_setschedprio(thA, -20);
+    pthread_setschedprio(thB, -19);
+    pthread_join(thA, NULL);
+    pthread_join(thB, NULL);
     return 0;
 }
