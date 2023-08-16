6984815b208b4efa87997e920da4f45527adf04b
Myriam <myriam.mabrouki@etu.sorbonne-universite.fr>
2023-08-16 13:29:57 +0200

    algo2 en cours d'avancement

--- QUAN_HU_Algorithm.c
+++ QUAN_HU_Algorithm.c
@@ -43,6 +43,13 @@ typedef struct _schedulingPoints {
     struct _schedulingPoints * next;
 } SchedulingPoints;
 
+typedef struct _job_TE_TL {
+    int TE;
+    int TL;
+    Job job;
+    struct _job_TE_TL * next;
+} Job_TE_TL;
+
 
 /* Computes the earliest scheduling point of a job Jn based on a set of jobs */
 int earliest_scheduling_point (Job j, Set s) {
@@ -95,7 +102,7 @@ float minimum_constant_speed (Set s, int ta, int tb) {
     return ((float) sum)/(tb-ta);
 } 
 
-SchedulingPoints * create_scheduling_point(SchedulingPoints * sp, int val) {
+SchedulingPoints * create_scheduling_point (SchedulingPoints * sp, int val) {
     SchedulingPoints * new = (SchedulingPoints *) malloc (sizeof(SchedulingPoints));
     new->val = val;
     new->next = sp;
@@ -120,18 +127,18 @@ Interval_and_speed essential_interval_Jn (int TE, int TL, Set s, Job * j) {
         ta = ta_prim;
         tb_prim = tb;
 
-        int min = -1;
+        int min_speed = -1;
         SchedulingPoints * curr = sp;
         while (curr != NULL) {
             if (curr->val > ta && curr->val <= TL) {
-                if (min == -1){
-                    min = minimum_constant_speed (s, ta, curr->val);
+                if (min_speed == -1){
+                    min_speed = minimum_constant_speed (s, ta, curr->val);
                     tb = curr->val;
                 }
                 else {
-                    int tmp = minimum_constant_speed (s, ta, curr->val);
-                    if (tmp < min) {
-                        min = tmp;
+                    int speed = minimum_constant_speed (s, ta, curr->val);
+                    if (speed < min_speed) {
+                        min_speed = speed;
                         tb = curr->val;
                     }
                 }
@@ -139,13 +146,13 @@ Interval_and_speed essential_interval_Jn (int TE, int TL, Set s, Job * j) {
             curr = curr->next;
         }
 
-        int max = -1;
+        int max_speed = -1;
         curr = sp;
         while (curr != NULL) {
             if (curr->val >= TE && curr->val <= ta) {
-                int tmp = minimum_constant_speed (s, curr->val, tb);
-                if (tmp > max) {
-                    max = tmp;
+                int speed = minimum_constant_speed (s, curr->val, tb);
+                if (speed > max_speed) {
+                    max_speed = speed;
                     ta_prim = curr->val;
                 }
             }
@@ -161,17 +168,119 @@ Interval_and_speed essential_interval_Jn (int TE, int TL, Set s, Job * j) {
     return res;
 }
 
+IntervalSet * create_interval_set (IntervalSet * is, Interval val) {
+    IntervalSet * new = (IntervalSet *) malloc (sizeof(IntervalSet));
+    new->interval = val;
+    new->next = is;
+    return new;
+}
+
+SpeedSet * create_speed_set (SpeedSet * ss, float val) {
+    SpeedSet * new = (SpeedSet *) malloc (sizeof(SpeedSet));
+    new->S = val;
+    new->next = ss;
+    return new;
+}
+
+Job_TE_TL * create_job_TE_TL (Job_TE_TL * j, int te, int tl, Job job) {
+    Job_TE_TL * new = (Job_TE_TL *) malloc (sizeof(Job_TE_TL));
+    new->TE = te;
+    new->TL = tl;
+    new->job = job;
+    new->next = j;
+    return new;
+}
+
 void critical_intervals (Set s) {
 
-    Job * current = s.head; //pointer on the current job
+    Job * current_job = s.head; //pointer on the current job
 
-    while (current != NULL) {
+    IntervalSet * A = NULL;
+    SpeedSet * S_prim = NULL;
+    Job_TE_TL * job_TE_TL = NULL;
+
+    while (current_job != NULL) {
         int TE, TL;
-        TE = earliest_scheduling_point(*current, s);
-        TL = latest_scheduling_point(*current);
-        Interval_and_speed tmp = essential_interval_Jn(TE, TL, s, current);
+        TE = earliest_scheduling_point(*current_job, s);
+        TL = latest_scheduling_point(*current_job);
 
-        current = current->next;
+        job_TE_TL = create_job_TE_TL(job_TE_TL, TE, TL, *current_job);
+        
+        Interval_and_speed tmp = essential_interval_Jn(TE, TL, s, current_job);
+        A = create_interval_set(A, tmp.interval);
+        S_prim = create_speed_set(S_prim, tmp.S);
+
+        current_job = current_job->next;
+    }
+
+    int k = 1;
+    IntervalSet * T = NULL;
+    SpeedSet * S = NULL;
+
+    while (A != NULL) {
+        
+
+        float S_j = S_prim->S;
+        Interval A_j = A->interval;
+
+        T = create_interval_set(T, A_j);
+        S = create_speed_set(S, S_j);
+
+        SpeedSet * tmp_speed = S_prim;
+        IntervalSet * tmp_interval = A;
+        A = A->next;
+        S_prim = S->next;
+        free(tmp_speed);
+        free(tmp_interval);
+
+        int m = 1;
+        current_job = s.head;
+        Job_TE_TL * curr_job_TE_TL = job_TE_TL;
+        while (current_job != NULL) {
+            if (m <= k && current_job->O >= A_j.ts && current_job->O < A_j.tf) {
+
+                //Remove Jm from J, the set of jobs
+                Job * tmp = current_job;
+                current_job = current_job->next;
+                free(tmp);
+
+                //Remove Sm from S and corresponding [ts, tf] from A
+                
+
+
+                //We also remove the job from job_TE_Tl because its release time, TE or TL won't be changed
+                Job_TE_TL * tmp2 = curr_job_TE_TL;
+                curr_job_TE_TL = curr_job_TE_TL->next;
+                free(tmp2);
+            }
+            else {
+                if (current_job->O > A_j.ts && current_job->O <= A_j.tf) {
+                    current_job->O = A_j.ts;
+                    curr_job_TE_TL->job.O = A_j.ts;
+                }
+                if (curr_job_TE_TL->TE > A_j.ts && curr_job_TE_TL->TE <= A_j.tf) {
+                    curr_job_TE_TL->TE = A_j.ts;
+                }
+                if (curr_job_TE_TL->TL > A_j.ts && curr_job_TE_TL->TL <= A_j.tf) {
+                    curr_job_TE_TL->TL = A_j.ts;
+                }
+
+                curr_job_TE_TL = curr_job_TE_TL->next;
+            }
+            current_job = current_job->next;
+        }
+
+        while (1) {
+
+        } 
+
+        curr_job_TE_TL = job_TE_TL;
+        while (curr_job_TE_TL != NULL) {
+
+            curr_job_TE_TL = curr_job_TE_TL->next;
+        }
+
+        k++;
     }    
 
 }
