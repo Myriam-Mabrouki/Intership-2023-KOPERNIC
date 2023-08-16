ccbd1ea2dd613a55146693e52ac094cacf20656b
Myriam <myriam.mabrouki@etu.sorbonne-universite.fr>
2023-08-17 01:47:27 +0200

    algo2 fini et testé (faire des tests avec d'autres job sets)

--- QUAN_HU_Algorithm.c
+++ QUAN_HU_Algorithm.c
@@ -10,23 +10,31 @@ typedef struct _job {
     int O; //Release time
     int C; //Computation time
     int D; //Absolute Deadline
-    struct _job * next; 
 } Job;
 
-typedef struct _set {
-    Job * head; //head of the list
-    int size; //number of jobs
-} Set;
+typedef struct _jobSet {
+    Job * job; //head of the list
+    struct _jobSet * next;
+} JobSet;
 
 typedef struct _interval {
     int ts;
     int tf;
 } Interval;
 
-typedef struct _interval_and_speed {
+typedef struct _set {
+    Job * job; 
+    int TE;
+    int TL;
+    float speed;
+    Interval essential_interval;
+    struct _set * next; 
+} Set;
+
+typedef struct _intervalSpeed {
     Interval interval;
     float S;
-} Interval_and_speed;
+} IntervalSpeed;
 
 typedef struct _intervalSet {
     Interval interval;
@@ -38,35 +46,72 @@ typedef struct _speedSet {
     struct _speedSet * next;
 } SpeedSet;
 
+typedef struct _intervalsSpeeds {
+    IntervalSet * intervalSet;
+    SpeedSet * speedSet;
+} IntervalsSpeeds;
+
 typedef struct _schedulingPoints {
     int val;
     struct _schedulingPoints * next;
 } SchedulingPoints;
 
-typedef struct _job_TE_TL {
-    int TE;
-    int TL;
-    Job job;
-    struct _job_TE_TL * next;
-} Job_TE_TL;
 
+IntervalSet * create_interval_set (IntervalSet * is, Interval interval) {
+    IntervalSet * new = (IntervalSet *) malloc (sizeof(IntervalSet));
+    new->interval = interval;
+    new->next = is;
+    return new;
+}
+
+SpeedSet * create_speed_set (SpeedSet * ss, float speed) {
+    SpeedSet * new = (SpeedSet *) malloc (sizeof(SpeedSet));
+    new->S = speed;
+    new->next = ss;
+    return new;
+}
+
+JobSet * create_job_set (JobSet * js, Job * job) {
+    JobSet * new = (JobSet *) malloc (sizeof(JobSet));
+    new->job = job;
+    new->next = js;
+    return new;
+}
+
+Set * create_set (Set * s, Job * job, int te, int tl, float speed, Interval interval) {
+    Set * new = (Set *) malloc (sizeof(Set));
+    new->job = job;
+    new->TE = te;
+    new->TL = tl;
+    new->speed = speed;
+    new->essential_interval = interval;
+    new->next = s;
+    return new;
+}
+
+SchedulingPoints * create_scheduling_point (SchedulingPoints * sp, int val) {
+    SchedulingPoints * new = (SchedulingPoints *) malloc (sizeof(SchedulingPoints));
+    new->val = val;
+    new->next = sp;
+    return new;
+}
 
 /* Computes the earliest scheduling point of a job Jn based on a set of jobs */
-int earliest_scheduling_point (Job j, Set s) {
+int earliest_scheduling_point (Job j, JobSet * js) {
     
-    Job * current = s.head; //pointer on the current job
-    Job * tmp = NULL; 
+    JobSet * current = js; //pointer on the current job
+    JobSet * tmp = NULL; 
     int TE = j.O; //We initialise the earliest scheduling point to the current job release
 
     while (current != NULL){
        
-        if (current->O >= 0 && current->O <= j.O) { //First, we check if the considered scheduling point is in [0, Rn]
+        if (current->job->O >= 0 && current->job->O <= j.O) { //First, we check if the considered scheduling point is in [0, Rn]
             
             int found = true;
             
-            tmp  = s.head;
+            tmp = js;
             while (tmp != NULL) {
-                if (current->O > tmp->O && current->O < tmp->D) {
+                if (current->job->O > tmp->job->O && current->job->O < tmp->job->D) {
                     found = false;
                     break;
                 }
@@ -74,7 +119,7 @@ int earliest_scheduling_point (Job j, Set s) {
             }
             
             if (found) {
-                if (current->O < TE) TE = current->O;
+                if (current->job->O < TE) TE = current->job->O;
             }
         }
         current = current->next;
@@ -87,14 +132,14 @@ int latest_scheduling_point (Job j) {
     return j.D;
 }
 
-float minimum_constant_speed (Set s, int ta, int tb) {
+float minimum_constant_speed (JobSet * js, int ta, int tb) {
     
-    Job * current = s.head; //pointer on the current job
+    JobSet * current = js; //pointer on the current job
     int sum = 0; //sum of the computation times in [ta, tb]
 
-    while (current) {
-        if (current->O >= ta && current->O < tb) { //We check if the current job is computed in [ta, tb]
-            sum += current->C;
+    while (current != NULL) {
+        if (current->job->O >= ta && current->job->O < tb) { //We check if the current job is computed in [ta, tb]
+            sum += current->job->C;
         }
         current = current->next;
     }
@@ -102,25 +147,20 @@ float minimum_constant_speed (Set s, int ta, int tb) {
     return ((float) sum)/(tb-ta);
 } 
 
-SchedulingPoints * create_scheduling_point (SchedulingPoints * sp, int val) {
-    SchedulingPoints * new = (SchedulingPoints *) malloc (sizeof(SchedulingPoints));
-    new->val = val;
-    new->next = sp;
-    return new;
-}
 
-Interval_and_speed essential_interval_Jn (int TE, int TL, Set s, Job * j) {
-    int ta_prim = j->O, tb = j->O;
+
+IntervalSpeed essential_interval_Jn (int TE, int TL, JobSet * js, Job j) {
+    int ta_prim = j.O, tb = j.O;
     int ta = TE;
     int tb_prim = TL;
 
-    Job * current = s.head;
+    JobSet * curr_job = js;
 
     //We create a set of j-scheduling points
-    SchedulingPoints * sp = create_scheduling_point(NULL, j->D);
-    while (current != NULL) {
-        sp = create_scheduling_point(sp, current->O);
-        current = current->next;
+    SchedulingPoints * sp = create_scheduling_point(NULL, j.D);
+    while (curr_job != NULL) {
+        sp = create_scheduling_point(sp, curr_job->job->O);
+        curr_job = curr_job->next;
     }
 
     while (ta != ta_prim || tb_prim != tb) {
@@ -128,174 +168,247 @@ Interval_and_speed essential_interval_Jn (int TE, int TL, Set s, Job * j) {
         tb_prim = tb;
 
         int min_speed = -1;
-        SchedulingPoints * curr = sp;
-        while (curr != NULL) {
-            if (curr->val > ta && curr->val <= TL) {
+        SchedulingPoints * curr_sp = sp;
+        while (curr_sp != NULL) {
+            if (curr_sp->val > ta && curr_sp->val <= TL) {
                 if (min_speed == -1){
-                    min_speed = minimum_constant_speed (s, ta, curr->val);
-                    tb = curr->val;
+                    min_speed = minimum_constant_speed (js, ta, curr_sp->val);
+                    tb = curr_sp->val;
                 }
                 else {
-                    int speed = minimum_constant_speed (s, ta, curr->val);
+                    int speed = minimum_constant_speed (js, ta, curr_sp->val);
                     if (speed < min_speed) {
                         min_speed = speed;
-                        tb = curr->val;
+                        tb = curr_sp->val;
                     }
                 }
             }
-            curr = curr->next;
+            curr_sp = curr_sp->next;
         }
 
         int max_speed = -1;
-        curr = sp;
-        while (curr != NULL) {
-            if (curr->val >= TE && curr->val <= ta) {
-                int speed = minimum_constant_speed (s, curr->val, tb);
+        curr_sp = sp;
+        while (curr_sp != NULL) {
+            if (curr_sp->val >= TE && curr_sp->val <= ta) {
+                int speed = minimum_constant_speed (js, curr_sp->val, tb);
                 if (speed > max_speed) {
                     max_speed = speed;
-                    ta_prim = curr->val;
+                    ta_prim = curr_sp->val;
                 }
             }
-            curr = curr->next;
+            curr_sp = curr_sp->next;
         }
     }
 
-    Interval_and_speed res;
+    IntervalSpeed res;
     res.interval.ts = ta;
     res.interval.tf = tb;
-    res.S = minimum_constant_speed(s, ta, tb);
+    res.S = minimum_constant_speed(js, ta, tb);
     
     return res;
 }
 
-IntervalSet * create_interval_set (IntervalSet * is, Interval val) {
-    IntervalSet * new = (IntervalSet *) malloc (sizeof(IntervalSet));
-    new->interval = val;
-    new->next = is;
-    return new;
+void print_interval (Interval i) {
+    printf("Interval - ts : %d, tf : %d", i.ts, i.tf);
 }
 
-SpeedSet * create_speed_set (SpeedSet * ss, float val) {
-    SpeedSet * new = (SpeedSet *) malloc (sizeof(SpeedSet));
-    new->S = val;
-    new->next = ss;
-    return new;
+void print_intervals (IntervalSet * arg) {
+    IntervalSet * i = arg;
+    while (i) {
+        print_interval(i->interval);
+        printf("\n");
+        i = i->next;
+    }
 }
 
-Job_TE_TL * create_job_TE_TL (Job_TE_TL * j, int te, int tl, Job job) {
-    Job_TE_TL * new = (Job_TE_TL *) malloc (sizeof(Job_TE_TL));
-    new->TE = te;
-    new->TL = tl;
-    new->job = job;
-    new->next = j;
-    return new;
+void print_speeds (SpeedSet * arg) {
+    SpeedSet * s = arg;
+    while (s) {
+        printf("Speed : %.2f\n", s->S);
+        s = s->next;
+    }
 }
 
-void critical_intervals (Set s) {
+void print_job (Job j) {
+    printf("Job - index : %d, O : %d, C : %d, D : %d", j.index, j.O, j.C, j.D);
+}
 
-    Job * current_job = s.head; //pointer on the current job
+void print_job_set (JobSet * arg) {
+    JobSet * js = arg;
+    while (js) {
+        print_job(*js->job);
+        if (js->next == NULL) {
+            printf(" - Last job\n");
+        }
+        else {
+            printf(" - Next job index : %d\n", js->next->job->index);
+        }
+        js = js->next;
+    }
+}
 
-    IntervalSet * A = NULL;
-    SpeedSet * S_prim = NULL;
-    Job_TE_TL * job_TE_TL = NULL;
+void print_set (Set * arg) {
+    Set * s = arg;
+    while (s) {
+        print_job(*s->job);
+        printf(", ");
+        print_interval(s->essential_interval);
+        printf(", Speed : %.2f, TE : %d, TL : %d\n", s->speed, s->TE, s->TL);
+        s = s->next;
+    }
+}
 
-    while (current_job != NULL) {
+IntervalsSpeeds critical_intervals (JobSet * js) {
+
+    JobSet * curr_job = js; //pointer on the current job
+    Set * set = NULL; //combination of A and S_prim
+
+    while (curr_job != NULL) {
         int TE, TL;
-        TE = earliest_scheduling_point(*current_job, s);
-        TL = latest_scheduling_point(*current_job);
+        TE = earliest_scheduling_point(*curr_job->job, js);
+        TL = latest_scheduling_point(*curr_job->job);
 
-        job_TE_TL = create_job_TE_TL(job_TE_TL, TE, TL, *current_job);
-        
-        Interval_and_speed tmp = essential_interval_Jn(TE, TL, s, current_job);
-        A = create_interval_set(A, tmp.interval);
-        S_prim = create_speed_set(S_prim, tmp.S);
+        IntervalSpeed interval_speed = essential_interval_Jn(TE, TL, js, *curr_job->job);
 
-        current_job = current_job->next;
+        set = create_set(set, curr_job->job, TE, TL, interval_speed.S, interval_speed.interval);
+
+        curr_job = curr_job->next;
     }
 
-    int k = 1;
     IntervalSet * T = NULL;
     SpeedSet * S = NULL;
 
-    while (A != NULL) {
+    while (set != NULL) {
         
+        //We select S_j, i.e. the maximum constant speed, and the corresponding interval 
+        float S_j = set->speed;
+        Interval A_j = set->essential_interval;
+        int j = set->job->index;
 
-        float S_j = S_prim->S;
-        Interval A_j = A->interval;
+        /*while (curr_set->next != NULL) {
+            if (S_j > curr_set->next->speed) {
+                S_j = curr_set->next->speed;
+                A_j = curr_set->next->essential_interval;
+                j = curr_set->next->job->index;
+                tmp = curr_set->next;
+                prec = curr_set;
+            }
+            curr_set = curr_set->next;
+        }*/
 
+        //We add S_j and [ts, tf] respectively to S and T
         T = create_interval_set(T, A_j);
         S = create_speed_set(S, S_j);
 
-        SpeedSet * tmp_speed = S_prim;
-        IntervalSet * tmp_interval = A;
-        A = A->next;
-        S_prim = S->next;
-        free(tmp_speed);
-        free(tmp_interval);
+        //We remove S_j and [ts, tf] respectively from S_prim and A
+        /* if (prec == NULL) {
+            set = set->next;
+            free(tmp);
+        }
+        else {
+            prec->next = tmp->next;
+            free(tmp);
+        } */
+        Set * tmp = set;
+        set = set->next;
+        free(tmp);
 
-        int m = 1;
-        current_job = s.head;
-        Job_TE_TL * curr_job_TE_TL = job_TE_TL;
-        while (current_job != NULL) {
-            if (m <= k && current_job->O >= A_j.ts && current_job->O < A_j.tf) {
-
-                //Remove Jm from J, the set of jobs
-                Job * tmp = current_job;
-                current_job = current_job->next;
-                free(tmp);
-
-                //Remove Sm from S and corresponding [ts, tf] from A
+        int m;
+        Set * curr_set = set;
+        Set * prec = NULL;
+        while (curr_set != NULL) {
+            m = curr_set->job->index;
+            if (m <= j && curr_set->job->O >= A_j.ts && curr_set->job->O < A_j.tf) {
                 
-
-
-                //We also remove the job from job_TE_Tl because its release time, TE or TL won't be changed
-                Job_TE_TL * tmp2 = curr_job_TE_TL;
-                curr_job_TE_TL = curr_job_TE_TL->next;
-                free(tmp2);
+                //Remove Jm from J, the set of jobs & Remove Sm from S and corresponding [ts, tf] from A
+                if (prec == NULL) {
+                    set = set->next;
+                    free(curr_set);
+                    curr_set = set;
+                }
+                else {
+                    prec->next = curr_set->next;
+                    free(curr_set);
+                    curr_set = prec->next;
+                }
             }
             else {
-                if (current_job->O > A_j.ts && current_job->O <= A_j.tf) {
-                    current_job->O = A_j.ts;
-                    curr_job_TE_TL->job.O = A_j.ts;
+                if (curr_set->job->O > A_j.ts && curr_set->job->O <= A_j.tf) {
+                    curr_set->job->O = A_j.ts;
                 }
-                if (curr_job_TE_TL->TE > A_j.ts && curr_job_TE_TL->TE <= A_j.tf) {
-                    curr_job_TE_TL->TE = A_j.ts;
+                if (curr_set->TE > A_j.ts && curr_set->TE <= A_j.tf) {
+                    curr_set->TE = A_j.ts;
                 }
-                if (curr_job_TE_TL->TL > A_j.ts && curr_job_TE_TL->TL <= A_j.tf) {
-                    curr_job_TE_TL->TL = A_j.ts;
+                if (curr_set->TL > A_j.ts && curr_set->TL <= A_j.tf) {
+                    curr_set->TL = A_j.ts;
                 }
 
-                curr_job_TE_TL = curr_job_TE_TL->next;
+                prec = curr_set;
+                curr_set = curr_set->next;
             }
-            current_job = current_job->next;
         }
 
-        while (1) {
 
+        curr_set = set;
+        while (curr_set != NULL) {
+            if (curr_set->job->O > A_j.tf) {
+                curr_set->job->O = curr_set->job->O - (A_j.tf - A_j.ts);
+            }
+            if (curr_set->job->D > A_j.tf) {
+                curr_set->job->D = curr_set->job->D - (A_j.tf - A_j.ts);
+            }
+            curr_set = curr_set->next;
         } 
 
-        curr_job_TE_TL = job_TE_TL;
-        while (curr_job_TE_TL != NULL) {
-
-            curr_job_TE_TL = curr_job_TE_TL->next;
+        curr_set = set;
+        while (curr_set != NULL) {
+            IntervalSpeed interval_speed = essential_interval_Jn (curr_set->TE, curr_set->TL, js, *curr_set->job);
+            curr_set->essential_interval = interval_speed.interval;
+            curr_set->speed = interval_speed.S;
+            curr_set = curr_set->next;
         }
+    } 
 
-        k++;
-    }    
+    IntervalsSpeeds res;
+    res.intervalSet = T;
+    res.speedSet = S;
 
+    return res;
+
+}
+
+void print_intervals_speeds (IntervalsSpeeds arg) {
+    SpeedSet * s = arg.speedSet;
+    IntervalSet * i = arg.intervalSet;
+
+    while (s && i) {
+        
+        printf("Speed : %.2f, Interval : ts - %d, tf - %d\n", s->S, i->interval.ts, i->interval.tf);
+
+        s = s->next;
+        i = i->next;
+    }
 }
 
 int main (int argc, char * argv[]) {
     
     //We create 3 jobs
-    Job j3 = {3, 3, 5, 10, NULL};
-    Job j2 = {2, 2, 4, 8, &j3};
-    Job j1 = {1, 0, 1, 9, &j2};
+    Job j3 = {3, 3, 5, 10};
+    Job j2 = {2, 2, 4, 8};
+    Job j1 = {1, 0, 1, 9};
 
-    Set s = {&j1, 3};
+    JobSet * js = NULL;
+    js = create_job_set(js, &j3);
+    js = create_job_set(js, &j2);
+    js = create_job_set(js, &j1);
 
-    Interval_and_speed tmp = essential_interval_Jn(earliest_scheduling_point(j3, s), latest_scheduling_point(j3), s, &j3);
-    printf("ts : %d, tf : %d, S : %.2f\n", tmp.interval.ts, tmp.interval.tf, tmp.S);
+    print_job_set(js);
+    printf("\n");
+
+    IntervalsSpeeds res = critical_intervals(js);
+    
+    printf("Final results: \n");
+    print_intervals_speeds(res);
 
     return 0;
 }
