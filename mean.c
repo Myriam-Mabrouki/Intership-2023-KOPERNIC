2a56f133d87187fa2ca1af1295c21aa0804ff1a8
Myriam Mabrouki <mmabrouk@ptb-02007046.paris.inria.fr>
2024-07-16 17:19:20 +0200

    new stats functions : ecart-type, etendue

--- mean.c
+++ mean.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <math.h> 
 
 #define MAX 256
 
@@ -23,14 +24,17 @@ char* substr(const char *src, int m, int n)
     return dest - len;
 }
 
+
 int main(int argc, char **argv)
 {
 	if (argc != 2){
-		fprintf (stderr, "Mauvais nombre d'arguments\n");
+		perror("Wrong number of arguments\n");
 		exit(EXIT_FAILURE);
 	}
 	
-	FILE * file_res = fopen("results.txt", "a"); 
+	FILE * file_average = fopen("results_average.txt", "a"); 
+	FILE * file_etendue = fopen("results_etendue.txt", "a"); 
+	FILE * file_ecarttype = fopen("results_ecarttype.txt", "a"); 
 
 	char dirname[18] = "perf_measures";
 	strcat(dirname, argv[1]);
@@ -39,10 +43,10 @@ int main(int argc, char **argv)
 	struct dirent *cur;
 
 	while ((cur = readdir(dir))) {
-		if (cur->d_name[0] != '.') {  //ne pas afficher les fichiers cachés
+		if (cur->d_name[0] != '.') {  //Don't display hidden files
 			
 			char str[MAX] = "";
-			float sum = 0;
+			float sum = 0, max = -1, min = -1, variance = 0, tmp;
 			int i = 0;
 
 			char * filename = (char *) malloc(sizeof(char) * (strlen(dirname)+strlen(cur->d_name)+2));
@@ -53,29 +57,40 @@ int main(int argc, char **argv)
 			FILE * file = fopen(filename, "r");
 
 			if (file == NULL) {
-				perror("FILE ERROR");
+				perror("Error while opening file");
 				exit(EXIT_FAILURE);
 			}
 	
 			while (fgets(str, MAX, file) != NULL){
-				
-				sum += atol((const char *) str);
+				tmp = atol((const char *) str)
+				if (max < tmp) max = tmp;
+				if (min == -1 || min > tmp) min = tmp;
+				sum += tmp;
 				i++;
-			
 			}
+
+			while (fgets(str, MAX, file) != NULL){
+				tmp = atol((const char *) str)
+				variance += ((tmp - (sum/i)) << 1);
+			}
+			variance /= i;
 			
 			fclose(file);
 			
 			free(filename);
 
-			fprintf(file_res, "%s %d  %f\n", substr(cur->d_name, 3, strlen(cur->d_name)-4), atoi(argv[1]), (sum/i));
+			fprintf(file_average, "%s %d  %f\n", substr(cur->d_name, 3, strlen(cur->d_name)-4), atoi(argv[1]), (sum/i));
+			fprintf(file_etendue, "%s %d  %f\n", substr(cur->d_name, 3, strlen(cur->d_name)-4), atoi(argv[1]), max-min);
+			fprintf(file_ecarttype, "%s %d  %f\n", substr(cur->d_name, 3, strlen(cur->d_name)-4), atoi(argv[1]), sqrt(variance));
 		}
 	}
 
 	closedir(dir);
 	
-	fclose(file_res);
-	 	
+	fclose(file_average);
+	fclose(file_etendue);
+	fclose(file_ecarttype);
+
 	return 0;
 }
 
