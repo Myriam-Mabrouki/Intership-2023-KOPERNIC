b3cfca524af9141f5dc39d564709db36c6891d6f
Myriam Mabrouki <mmabrouk@ptb-02007046.paris.inria.fr>
2024-08-16 16:47:46 +0200

    UART + Time fixed

--- Scheduler/Job_Scheduler.c
+++ Scheduler/Job_Scheduler.c
@@ -5,34 +5,34 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <errno.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <string.h>
 #include <termios.h>
 #include <time.h>
 
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/adpcm_dec/adpcm_dec.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/adpcm_enc/adpcm_enc.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/ammunition/ammunition.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/anagram/anagram.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/audiobeam/audiobeam.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/cjpeg_transupp/cjpeg_transupp.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/cjpeg_wrbmp/cjpeg_wrbmp.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/dijkstra/dijkstra.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/epic/epic.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/fmref/fmref.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/g723_enc/g723_enc.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/gsm_dec/gsm_dec.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/gsm_enc/gsm_enc.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/h264_dec/h264_dec.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/huff_dec/huff_dec.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/huff_enc/huff_enc.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/mpeg2/mpeg2.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/ndes/ndes.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/petrinet/petrinet.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/rijndael_dec/rijndael_dec.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/rijndael_enc/rijndael_enc.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/statemate/statemate.h"
-#include "/home/mmabrouk/Documents/tacle-bench/bench/librairies/susan/susan.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/adpcm_dec/adpcm_dec.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/adpcm_enc/adpcm_enc.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/ammunition/ammunition.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/anagram/anagram.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/audiobeam/audiobeam.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/cjpeg_transupp/cjpeg_transupp.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/cjpeg_wrbmp/cjpeg_wrbmp.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/dijkstra/dijkstra.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/epic/epic.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/fmref/fmref.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/g723_enc/g723_enc.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/gsm_dec/gsm_dec.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/gsm_enc/gsm_enc.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/h264_dec/h264_dec.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/huff_dec/huff_dec.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/huff_enc/huff_enc.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/mpeg2/mpeg2.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/ndes/ndes.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/petrinet/petrinet.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/rijndael_dec/rijndael_dec.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/rijndael_enc/rijndael_enc.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/statemate/statemate.h"
+#include "/home/mabrouki/Documents/tacle-bench/bench/librairies/susan/susan.h"
 
 int max (int a, int b)
 {
@@ -44,7 +44,7 @@ int set_interface_attribs (int fd, int speed, int parity)
         struct termios tty;
         if (tcgetattr (fd, &tty) != 0)
         {
-                //perror ("error %d from tcgetattr", errno);
+                fprintf(stderr, "error %d from tcgetattr\n", errno);
                 return -1;
         }
 
@@ -72,7 +72,7 @@ int set_interface_attribs (int fd, int speed, int parity)
 
         if (tcsetattr (fd, TCSANOW, &tty) != 0)
         {
-                //perror ("error %d from tcsetattr", errno);
+                fprintf(stderr, "error %d from tcsetattr\n", errno);
                 return -1;
         }
         return 0;
@@ -83,7 +83,7 @@ void set_blocking (int fd, int should_block)
         struct termios tty;
         memset (&tty, 0, sizeof tty);
         if (tcgetattr (fd, &tty) != 0) {
-            //perror ("error %d from tggetattr", errno);
+            fprintf(stderr, "error %d from tggetattr\n", errno);
             return;
         }
 
@@ -94,19 +94,19 @@ void set_blocking (int fd, int should_block)
             perror ("error %d setting term attributes", errno); */
 }
 
-int main () 
+int main ()
 {
 
-    char *portname = "/dev/ttyS0";
+    char *portname = "/dev/ttyAMA0";
 
-    //int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
-    /* if (fd < 0){
-        //perror ("error %d opening %s: %s", errno, portname, strerror (errno));
+    int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0){
+        fprintf (stderr, "error %d opening %s: %s\n", errno, portname, strerror (errno));
         return -1;
     }
-     */
-    //set_interface_attribs (fd, 9600, 0);  // set speed to 115,200 bps, 8n1 (no parity)
-    //set_blocking (fd, 0);                // set no blocking
+
+    set_interface_attribs (fd, 115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+    set_blocking (fd, 0);                // set no blocking
 
     /* main_adpcm_dec(); //OK
     main_adpcm_enc(); //OK
@@ -131,13 +131,13 @@ int main ()
     main_rijndael_enc(); //OK
     main_statemate(); //OK
     main_susan(); //OK (do not use, assign CPU is not working) */
-    
+
     pthread_t thA, thB, thC;
     clock_t start_t;
     struct timespec remaining_time;
 
-    for(int i = 0; i < 500; i++){
-        //write (fd, "begin\n", 7); //UART communication, beginnning of the loop
+    for(int i = 0; i < 1000; i++){
+        write (fd, "loop\n", 6); //UART communication, beginnning of the loop
 
         start_t = clock();
 
@@ -147,22 +147,19 @@ int main ()
         pthread_create(&thB, NULL, (void *) main_ndes, NULL);
         pthread_setschedprio(thB, -19);
         pthread_create(&thC, NULL, (void *) main_cjpeg_wrbmp, NULL);
-        
+
         /* Check if there is a deadline miss */
-        //remaining_time.tv_sec  = 1 - ((clock() - start_t)/CLOCKS_PER_SEC);
+
         remaining_time.tv_nsec = 4930000 - (((clock() - start_t)/CLOCKS_PER_SEC)*1000000000);
-        printf("%ld %09ld\n", remaining_time.tv_sec, remaining_time.tv_nsec);
-        
-        if (pthread_timedjoin_np(thA, NULL, &remaining_time) != 0 && pthread_timedjoin_np(thB, NULL, &remaining_time) != 0 && pthread_timedjoin_np(thC, NULL, &remaining_time) != 0){
+
+        if (pthread_timedjoin_np(thA, NULL, &remaining_time) != 0 && pthread_timedjoin_np(thB, NULL, &remaining_time) != 0 && pthread_timedjoin_np(thC, NULL, &remaining_time) !=  0){
             printf("Deadline miss !\n");
-            exit(EXIT_FAILURE); 
+            exit(EXIT_FAILURE);
         }
+    }
 
-        //write (fd, "end\n", 5); //UART communication, end of the loop
-    } 
+    char buf [6000];
+    read (fd, buf, sizeof buf);  // read up to 500 characters if ready to read
 
-    char buf [500];
-    //int n = read (fd, buf, sizeof buf);  // read up to 500 characters if ready to read  
-    
     return 0;
 }
